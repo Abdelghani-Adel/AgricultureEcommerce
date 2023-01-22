c88d7f5e58bf5319128a4eb92ba5057b347a6cee
AbdelghaniAdel <Heba@LAPRTS-02>
2023-01-22 14:33:58 +0200

    Arranged the Footer component

--- components/layout/Footer/Footer.js
+++ components/layout/Footer/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Link from "next/Link";
 import { FaChevronUp } from "react-icons/fa";
 import { withTranslation } from "react-multi-lang";
-import { FaFacebookF, FaTwitter, FaYoutube ,FaHandPointer } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaYoutube, FaHandPointer } from "react-icons/fa";
 class Footer extends Component {
   scrollToTop() {
     window.scrollTo({
@@ -13,108 +13,107 @@ class Footer extends Component {
   render() {
     return (
       <footer className={"andro_footer " + this.props.footer.style}>
-        {/* Top Footer */}
+        {/* Logo */}
         <div className="container">
           <div className="andro_footer-top">
             <Link href="/" className="andro_footer-logo">
               <img src="../img/logo.png" alt="logo" />
             </Link>
-            {/* <div className="andro_footer-buttons">
-                            <Link href="#"> <img src={"img/android.png"} alt="download it on the app store" /></Link>
-                            <Link href="#"> <img src={"img/ios.png"} alt="download it on the app store" /></Link>
-                        </div> */}
           </div>
         </div>
         {/* Middle Footer */}
         <div className="andro_footer-middle">
           <div className="container">
             <div className="row">
+              {/* Information */}
               <div className="col-xl-3 col-lg-3 col-md-4 col-sm-12 footer-widget">
                 <h5 className="widget-title">{this.props.t("Footer.Information")}</h5>
                 <ul>
                   <li>
-                    {" "}
-                    <Link href="/">Home</Link>{" "}
+                    <Link href="/">Home</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/blog-grid">Blog</Link>{" "}
+                    <Link href="/blog-grid">Blog</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/about">About Us</Link>{" "}
+                    <Link href="/about">About Us</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/shop-v1">Shop</Link>{" "}
+                    <Link href="/shop-v1">Shop</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/contact">Contact Us</Link>{" "}
+                    <Link href="/contact">Contact Us</Link>
                   </li>
                 </ul>
               </div>
+
+              {/* Tpop Categories */}
               <div className="col-xl-3 col-lg-3 col-md-4 col-sm-12 footer-widget">
                 <h5 className="widget-title">{this.props.t("Footer.TopCategories")}</h5>
                 {/* <ul>
-                                    <li> <Link href="#">Food</Link> </li>
-                                    <li> <Link href="#">Baskets</Link> </li>
-                                    <li> <Link href="#">Supplements</Link> </li>
-                                    <li> <Link href="#">Keto</Link> </li>
-                                    <li> <Link href="#">Home Care</Link> </li>
-                                </ul> */}
+                  <li>
+                    <Link href="#">Food</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Baskets</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Supplements</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Keto</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Home Care</Link>
+                  </li>
+                </ul> */}
               </div>
+
+              {/* Others */}
               <div className="col-xl-3 col-lg-3 col-md-4 col-sm-12 footer-widget">
                 <h5 className="widget-title">{this.props.t("Footer.Others")}</h5>
                 <ul>
                   <li>
-                    {" "}
-                    <Link href="/checkout">Checkout</Link>{" "}
+                    <Link href="/checkout">Checkout</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/cart">Cart</Link>{" "}
+                    <Link href="/cart">Cart</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/product-single">Product</Link>{" "}
+                    <Link href="/product-single">Product</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/shop-v1">Shop</Link>{" "}
+                    <Link href="/shop-v1">Shop</Link>
                   </li>
                   <li>
-                    {" "}
-                    <Link href="/legal">Legal</Link>{" "}
+                    <Link href="/legal">Legal</Link>
                   </li>
                 </ul>
               </div>
+
+              {/* Social Media */}
               <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12 footer-widget">
                 <h5 className="widget-title">{this.props.t("Footer.SocialMedia")}</h5>
                 <ul className="social-media">
                   <li>
-                    {" "}
                     <Link href="#" className="facebook">
-                      {" "}
-                      <FaFacebookF />{" "}
-                    </Link>{" "}
+                      <FaFacebookF />
+                    </Link>
                   </li>
 
                   <li>
-                    {" "}
                     <Link href="#" className="google">
-                      {" "}
-                      <FaYoutube />{" "}
-                    </Link>{" "}
+                      <FaYoutube />
+                    </Link>
                   </li>
                   <li>
-                    {" "}
                     <Link href="#" className="twitter">
-                      {" "}
-                      <FaTwitter />{" "}
-                    </Link>{" "}
+                      <FaTwitter />
+                    </Link>
                   </li>
                 </ul>
+
+                {/* Signup button */}
                 {/* <div className="andro_footer-offer">
                   <p>{this.props.t("Footer.Signup")}</p>
                   <Link href="/register" className="andro_btn-custom btn-sm shadow-none">
@@ -130,32 +129,29 @@ class Footer extends Component {
           <div className="container">
             <ul>
               <li>
-                {" "}
-                <Link href="#">Privacy Policy</Link>{" "}
+                <Link href="#">Privacy Policy</Link>
               </li>
               <li>
-                {" "}
-                <Link href="#">Refund Policy</Link>{" "}
+                <Link href="#">Refund Policy</Link>
               </li>
               <li>
-                {" "}
-                <Link href="#">Cookie Policy</Link>{" "}
+                <Link href="#">Cookie Policy</Link>
               </li>
               <li>
-                {" "}
-                <Link href="#">Terms &amp; Conditions</Link>{" "}
+                <Link href="#">Terms &amp; Conditions</Link>
               </li>
             </ul>
+
             <div className="andro_footer-copyright">
               <p>
-                {" "}
-                Copyright © 2023 <Link href="#">RTS</Link> All Rights Reserved.{" "}
+                Copyright © 2023 <Link href="#">RTS</Link> All Rights Reserved.
               </p>
+
+              {/* Back to top */}
               <Link href="#" className="andro_back-to-top" onClick={() => this.scrollToTop()}>
-                {/* Back to top{" "} */}
                 <span>
                   <FaHandPointer />
-                </span>{" "}
+                </span>
               </Link>
             </div>
           </div>
--- pages/product-single/[productID].js
+++
@@ -1,9 +0,0 @@
-const ProductSingle = (props) => {
-  return (
-    <div className="container text-center p-4">
-      <h5>This page will be available soon</h5>
-    </div>
-  );
-};
-
-export default ProductSingle;
