49a1c134847f01e8412cef6a94c11cfe0abe322b
Rana <rana@rockettravelsystem.com>
2023-01-22 10:51:21 +0200

    update

--- components/layout/MainHeader/HeaderBottom/HeaderBottom.js
+++ components/layout/MainHeader/HeaderBottom/HeaderBottom.js
@@ -1,12 +1,16 @@
 import HeaderNav from "./HeaderNav";
 import SideCategoriesToggler from "./SideCategoriesToggler";
-
+import HeaderOffers from "./HeaderOffers";
 const HeaderBottom = (props) => {
   return (
     <div className="andro_header-bottom">
       <div className="andro_header-bottom-inner mt-0">
         <SideCategoriesToggler />
         <HeaderNav />
+        <div className="header_left_offers">
+          <HeaderOffers />
+
+        </div>
       </div>
     </div>
   );
