831af24a2d8ec7e77ba4dd2f9f4001ce5542b59c
AbdelghaniAdel <Heba@LAPRTS-02>
2023-02-13 10:49:10 +0200

    404 page

---
+++ pages/404.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const PageNotFound = (props) => {
+  return (
+    <div className="page_not_found">
+      <div className="section">
+        <div className="container">
+          <div className="andro_404-container">
+            <img src="assets/img/404.jpg" alt="" />
+            <h1>Page Not Found</h1>
+            <p>
+              The page you are trying to access could not be found. Please try looking for something
+              else
+            </p>
+            <Link href="/" className="btn btn-success mt-3">
+              Go To Home Page
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
