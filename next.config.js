1d47e9fe03f6e0c5cef9bf6868e421154345e3f3
AbdelghaniAdel <Heba@LAPRTS-02>
2023-02-13 13:09:29 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-    env: {
-      NEXTAUTH_URL: "https://devsdiamond.com",
-      NEXTAUTH_JWT_SECRET: "8F8D1546C2C1A23FECE7FCEE13E542DCA4F4B6613A072DE63B7F7F9C1F13263F",
-      NEXTAUTH_PROVIDERS: ["github", "google", "facebook", "CredentialsProvider"],
-    },
+  },
+  env: {
+    NEXTAUTH_URL: "https://devsdiamond.com",
+    NEXTAUTH_JWT_SECRET: "8F8D1546C2C1A23FECE7FCEE13E542DCA4F4B6613A072DE63B7F7F9C1F13263F",
+    NEXTAUTH_PROVIDERS: ["github", "google", "facebook", "CredentialsProvider"],
   },
 };
 
