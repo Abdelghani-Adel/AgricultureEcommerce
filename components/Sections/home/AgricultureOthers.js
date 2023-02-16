6d83482d7d34c1a856eb1d3915389bce90aa75e0
AbdelghaniAdel <Heba@LAPRTS-02>
2023-02-16 13:55:34 +0200

    Fetched books from api

--- components/Sections/home/AgricultureOthers.js
+++ components/Sections/home/AgricultureOthers.js
@@ -1,9 +1,37 @@
 import React, { Component, useState } from "react";
 import Link from "next/Link";
 import { withTranslation } from "react-multi-lang";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import ProductCardList from "../products/ProductCardList/ProductCardList";
 
 const AgricultureOthers = (props) => {
+  const lang = useSelector((state) => state.lang);
   const [booksList, setBooksList] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_SERVER}/api/ECommerceSetting/getBooksItems`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            lang: lang,
+            Cate_Id: 0,
+            Cust_Id: 0,
+            limit: 3,
+            start: 0,
+          }),
+        }
+      );
+
+      const data = await res.json();
+      setBooksList(data.data);
+    };
+
+    fetchBooks();
+  }, []);
   const { BooksInfoList } = props;
   return (
     <>
@@ -11,8 +39,9 @@ const AgricultureOthers = (props) => {
         <h4 className="title">{props.t("Products.BooksAndInfo")}</h4>
       </div>
       <div className="row">
+        <ProductCardList products={booksList} />
         {/* Product Start */}
-        {BooksInfoList.slice(0, 3).map((item, i) => (
+        {/* {BooksInfoList.slice(0, 3).map((item, i) => (
           <div className="col-lg-4 col-xs-12" key={i}>
             <div className="andro_product andro_product-minimal andro_product-has-controls andro_product-has-buttons">
               <div className="andro_product-thumb">
@@ -32,7 +61,7 @@ const AgricultureOthers = (props) => {
               </div>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
 
       <Link href="#" className="agri-btn-rounded-dark">
